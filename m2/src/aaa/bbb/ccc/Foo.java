b2055d28c10b096bfee81123aee54786bd7d7f8b
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-10-17 18:00:27 +0300

    Initial commit

---
+++ m2/src/aaa/bbb/ccc/Foo.java
@@ -0,0 +1,13 @@
+package aaa.bbb.ccc;
+
+import a.b.c.Bar;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/13/2017.
+ * Project: unloading
+ * *******************************
+ */
+public class Foo {
+    Bar bar;
+}
---
+++ m4/src/a/b/c/Bar.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/13/2017.
+ * Project: unloading
+ * *******************************
+ */
+public class Bar {
+}
