59272bf62599448c928f5c756801aa91e26b593e
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-10-17 18:23:14 +0300

    modify m5

---
+++ m5/tests/a/b/c/CCC1Test.java
@@ -0,0 +1,13 @@
+package a.b.c;
+
+import static org.junit.Assert.*;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 10/17/2017.
+ * Project: unloading
+ * *******************************
+ */
+public class CCC1Test {
+
+}
