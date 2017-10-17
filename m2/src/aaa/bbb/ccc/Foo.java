f54a144bd603f8eb8f7d5498efd5081790236acc
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-10-17 18:12:23 +0300

    add dependencies

--- m2/src/aaa/bbb/ccc/Foo.java
+++ m2/src/aaa/bbb/ccc/Foo.java
@@ -1,5 +1,8 @@
 package aaa.bbb.ccc;
 
+
+import a.b.c.AAA1;
+import a.b.c.BBB1;
 import a.b.c.Bar;
 
 /**
@@ -10,4 +13,6 @@ import a.b.c.Bar;
  */
 public class Foo {
     Bar bar;
+    AAA1 aaa1;
+    BBB1 bbb1;
 }
